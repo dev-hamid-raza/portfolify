685df4bc92dc646edc1149e7cecda827a9669cc1
Hamid Raza <hamidramay2002@gmail.com>
2024-08-18 22:15:41 +0500

    add accessToken endpoint

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logOutUser, loginUser, registerUser } from "../controllers/user.controller.js";
+import { logOutUser, loginUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import passport from 'passport';
 import '../auth/passport.js'; 
 import {verifyJWT} from '../middlewares/auth.middleware.js'
@@ -14,7 +14,7 @@ router.route('/login').post(loginUser)
 
 // Secure routes
 router.route('/logout').post(verifyJWT, logOutUser)
-
+router.route('/refresh-token').post(refreshAccessToken)
 // GitHub authentication routes
 
 
